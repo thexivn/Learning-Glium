c419e69e4af610fb8c0325b58c3b8206003594cc
thexivn <ivan.ghezzi.tux@gmail.com>
2019-11-17 12:12:54 +0100

    Tutorial 02 -- Drawing vertex and shaders

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
+#[macro_use]
 extern crate glium;
 
 use glium::{
-    glutin::{ContextBuilder, EventsLoop, WindowBuilder, Event, WindowEvent},
-    Display,
-    Surface
+    glutin::{ContextBuilder, Event, EventsLoop, WindowBuilder, WindowEvent},
+    index::{NoIndices, PrimitiveType},
+    Display, Program, Surface, VertexBuffer,
 };
+
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+}
 
 fn main() {
     let mut events_loop = EventsLoop::new();
@@ -12,21 +18,68 @@ fn main() {
     let context_builder = ContextBuilder::new();
     let display = Display::new(window_builder, context_builder, &events_loop).unwrap();
 
+    implement_vertex!(Vertex, position);
+
+    let vertex1 = Vertex {
+        position: [-0.5, -0.5],
+    };
+    let vertex2 = Vertex {
+        position: [0.0, 0.5],
+    };
+    let vertex3 = Vertex {
+        position: [0.5, -0.5],
+    };
+    let shape = vec![vertex1, vertex2, vertex3];
+
+    let vertex_buffer = VertexBuffer::new(&display, &shape).unwrap();
+    let indices = NoIndices(PrimitiveType::TrianglesList);
+
+    let vertex_shader_src = r#"
+        #version 140
+
+        in vec2 position;
+
+        void main() {
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 140
+
+        out vec4 color;
+
+        void main() {
+            color = vec4(1.0, 0.0, 0.0, 0.5);
+        }
+    "#;
+
+    let program =
+        Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+
     let mut closed = false;
     while !closed {
-
         let mut target = display.draw();
-        target.clear_color(0.0, 0.0, 0.0, 1.0);
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+
+        target
+            .draw(
+                &vertex_buffer,
+                &indices,
+                &program,
+                &glium::uniforms::EmptyUniforms,
+                &Default::default(),
+            )
+            .unwrap();
+
         target.finish().unwrap();
-        
-        events_loop.poll_events(|ev| {
-            match ev {
-                Event::WindowEvent {event, ..} => match event {
-                    WindowEvent::CloseRequested => closed = true,
-                    _ => ()
-                },
+
+        events_loop.poll_events(|ev| match ev {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => closed = true,
                 _ => (),
-            }
+            },
+            _ => (),
         });
     }
 }
